44e8d0cd1625d62ff3a43b540014018ed86cdbbc
carlabalbontin <carlabalbontin@hotmail.com>
2019-10-29 14:52:27 -0300

    Subjects with 2 observers

--- src/code.ts
+++ src/code.ts
@@ -14,6 +14,19 @@ subject.subscribe(
 subject.next('The first thing has been sent');
 
 
+//Second observer
+var observer2 = subject.subscribe(
+    data => addItem("Observer 2: " + data)
+)
+
+subject.next('The second thing has been sent');
+subject.next('A third thing has been sent');
+
+observer2.unsubscribe();
+
+subject.next('A final thing has been sent');
+
+
 function addItem(val:any) {
     var node = document.createElement("li");
     var textnode = document.createTextNode(val);
