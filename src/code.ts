f6203e8123796a91be5b4c378a77fe9840c9e301
carlabalbontin <carlabalbontin@hotmail.com>
2019-10-30 09:51:22 -0300

    Async Subject

--- src/code.ts
+++ src/code.ts
@@ -1,24 +1,24 @@
-import { ReplaySubject } from "rxjs/ReplaySubject";
+import { AsyncSubject } from "rxjs/AsyncSubject";
 
 //Create a new subject
-var replaySubject = new ReplaySubject(30, 2000);
+var asyncSubject = new AsyncSubject();
 
 //Create a subscription
-replaySubject.subscribe(
+asyncSubject.subscribe(
     data => addItem("Observer 1: " + data),
-    err => addItem("Error: " + err),
     () => addItem("Observer 1 Completed")
 )
 
 var i = 1;
-var int = setInterval( () => replaySubject.next(i++), 1000);
+var int = setInterval( () => asyncSubject.next(i++), 1000);
 
 
 setTimeout( () => {
-    var observer2 = replaySubject.subscribe(
+    var observer2 = asyncSubject.subscribe(
         data => addItem("Observer 2: " + data)
     )
-}, 5000);
+    asyncSubject.complete()
+}, 5000);  
 
 function addItem(val:any) {
     var node = document.createElement("li");
