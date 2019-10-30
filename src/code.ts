34598da74fca00026a2541d951b5971ba6ab7b8b
carlabalbontin <carlabalbontin@hotmail.com>
2019-10-30 09:29:22 -0300

    Behavior Subject - Real

--- src/code.ts
+++ src/code.ts
@@ -1,34 +1,33 @@
 import { Subject } from "rxjs/Subject";
-import { ReplaySubject } from "rxjs/ReplaySubject";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 //Create a new subject
 var subject = new Subject();
-var replaySubject = new ReplaySubject(2);
+var behaviorSubject = new BehaviorSubject('First');
 
 //Create a subscription
-replaySubject.subscribe(
+behaviorSubject.subscribe(
     data => addItem("Observer 1: " + data),
     err => addItem("Error: " + err),
     () => addItem("Observer 1 Completed")
 )
 
 //Emit values
-replaySubject.next('The first thing has been sent');
-replaySubject.next('Another thing has been sent');
-replaySubject.next('Observer 2 is about to subscribe...');
+behaviorSubject.next('The first thing has been sent');
+behaviorSubject.next('Observer 2 is about to subscribe...');
 
 
 //Second observer
-var observer2 = replaySubject.subscribe(
+var observer2 = behaviorSubject.subscribe(
     data => addItem("Observer 2: " + data)
 )
 
-replaySubject.next('The second thing has been sent');
-replaySubject.next('A third thing has been sent');
+behaviorSubject.next('The second thing has been sent');
+behaviorSubject.next('A third thing has been sent');
 
 observer2.unsubscribe();
 
-replaySubject.next('A final thing has been sent');
+behaviorSubject.next('A final thing has been sent');
 
 
 function addItem(val:any) {
