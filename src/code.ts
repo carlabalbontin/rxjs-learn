bdb19c8ff49b901b36fe305d818c0327d78ff57f
carlabalbontin <carlabalbontin@hotmail.com>
2019-10-30 11:01:31 -0300

    Map Operator Example

--- src/code.ts
+++ src/code.ts
@@ -1,17 +1,11 @@
 import { Observable } from "rxjs/Observable";
-import { merge } from "rxjs/observable/merge";
+import "rxjs/add/operator/map";
 
-var observable1 = Observable.create( (observer:any) => {
-    observer.next("Hey guys!");
-});
-
-var observable2 = Observable.create( (observer:any) => {
-    observer.next("How is it going?");
-});
-
-var newObservable = merge(observable1, observable2);
-
-newObservable.subscribe( (x:any) => addItem(x) );
+Observable.create( (observer:any) => {
+        observer.next("Hey guys!");
+    })
+    .map( (val:any) => val.toUpperCase() )
+    .subscribe( (x:any) => addItem(x) )
 
 
 function addItem(val:any) {
